0447f1f176ce6188ab932c93de06a5df50b9a490
git418064093 <418064093@qq.com>
2017-07-13 11:58:36 +0800

    1366px下 项目详情布局样式问题修改

--- app/config/env.js
+++ app/config/env.js
@@ -16,7 +16,7 @@ let hostConfig = {
         // origin: 'http://172.23.1.11:8080' // 詹铁强
         // origin: 'http://localhost:8080' // 本地服务器
         // origin: 'http://agent_dev.ipx.net' // 开发服务器
-        origin: 'http://agent_t.ipx.net' // 测试服务器
+        origin: 'https://agent_t.ipx.net' // 测试服务器
         // origin: 'https://agent.ipx.net' // 正式服务器
     }
 };
